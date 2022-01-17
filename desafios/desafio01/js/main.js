7d011a2f4f736ac0c69f99224a227f26808c4e92
Sagyo <notsagyo@gmail.com>
2022-01-17 17:34:19 -0300

    Add a few input checks

--- desafios/desafio01/js/main.js
+++ desafios/desafio01/js/main.js
@@ -1,7 +1,7 @@
 // Fibonacci (for)
 let input = prompt('Iterations of Fibonacci:');
 
-if (!isNaN(input) && input != null){
+if (!isNaN(input) && input != null && input != '') {
 	let currentN = 1;
 	let previousN = 0;
 	let result = 1;
@@ -24,7 +24,7 @@ if (!isNaN(input) && input != null){
 // Find in Fibonacci (while)
 input = prompt('Enter a number to find if it\'s a Fibonacci number');
 
-if (!isNaN(input) && input != null) {
+if (!isNaN(input) && input != null && input != '') {
 	let found = false;
 	let currentN = 1;
 	let previousN = 0;
@@ -59,7 +59,7 @@ input = prompt('Enter a positive number to count its digits');
 if (input > Number.MAX_SAFE_INTEGER) {
 	alert('The input exceeds 2^53-1 please try a smaller number');
 	console.log('The input exceeds 2^53-1 please try a smaller number');
-} else if (!isNaN(input) && input != null) {
+} else if (!isNaN(input) && input != null && input != '') {
 	let digits = 0;
 	let currentN = input;
 
@@ -77,84 +77,86 @@ if (input > Number.MAX_SAFE_INTEGER) {
 }
 
 // Days in a month (switch)
-input = prompt('Enter a month to know how many days has (inglés o español)');
-input = input.toLowerCase();
+input = prompt('Enter a month to know how many days it has (inglés o español)');
 
-switch (input) {
-	case 'january':
-	case 'enero':
-		alert('January has 31 days.');
-		console.log('January has 31 days');
-		break;
+if (input != null) {
+	input = input.toLowerCase();
+	switch (input) {
+		case 'january':
+		case 'enero':
+			alert('January has 31 days.');
+			console.log('January has 31 days');
+			break;
 
-	case 'february':
-	case 'febrero':
-		alert('February has 28 days (29 in leap years).');
-		console.log('February has 28 days (29 in leap years).');
-		break;
+		case 'february':
+		case 'febrero':
+			alert('February has 28 days (29 in leap years).');
+			console.log('February has 28 days (29 in leap years).');
+			break;
 
-	case 'march':
-	case 'marzo':
-		alert('March has 31 days');
-		console.log('March has 31 days');
-		break;
+		case 'march':
+		case 'marzo':
+			alert('March has 31 days');
+			console.log('March has 31 days');
+			break;
 
-	case 'april':
-	case 'abril':
-		alert('April has 30 days');
-		console.log('April has 30 days');
-		break;
+		case 'april':
+		case 'abril':
+			alert('April has 30 days');
+			console.log('April has 30 days');
+			break;
 
-	case 'may':
-	case 'mayo':
-		alert('May has 31 days');
-		console.log('May has 31 days');
-		break;
+		case 'may':
+		case 'mayo':
+			alert('May has 31 days');
+			console.log('May has 31 days');
+			break;
 
-	case 'june':
-	case 'junio':
-		alert('June has 30 days');
-		console.log('June has 30 days');
-		break;
+		case 'june':
+		case 'junio':
+			alert('June has 30 days');
+			console.log('June has 30 days');
+			break;
 
-	case 'july':
-	case 'julio':
-		alert('July has 31 days');
-		console.log('July has 31 days');
-		break;
+		case 'july':
+		case 'julio':
+			alert('July has 31 days');
+			console.log('July has 31 days');
+			break;
 
-	case 'august':
-	case 'agosto':
-		alert('August has 31 days');
-		console.log('August has 31 days');
-		break;
+		case 'august':
+		case 'agosto':
+			alert('August has 31 days');
+			console.log('August has 31 days');
+			break;
 
-	case 'september':
-	case 'septiembre':
-		alert('September has 30 days');
-		console.log('September has 30 days');
-		break;
+		case 'september':
+		case 'septiembre':
+			alert('September has 30 days');
+			console.log('September has 30 days');
+			break;
 
-	case 'october':
-	case 'octubre':
-		alert('October has 31 days');
-		console.log('October has 31 days');
-		break;
+		case 'october':
+		case 'octubre':
+			alert('October has 31 days');
+			console.log('October has 31 days');
+			break;
 
-	case 'november':
-	case 'noviembre':
-		alert('November has 30 days');
-		console.log('November has 30 days');
-		break;
+		case 'november':
+		case 'noviembre':
+			alert('November has 30 days');
+			console.log('November has 30 days');
+			break;
 
-	case 'december':
-	case 'diciembre':
-		alert('December has 31 days');
-		console.log('December has 31 days');
-		break;
+		case 'december':
+		case 'diciembre':
+			alert('December has 31 days');
+			console.log('December has 31 days');
+			break;
 
-	default:
-		alert('Invalid input!');
-		console.log('Invalid input!');
-		break;
+		default:
+			alert('Invalid input!');
+			console.log('Invalid input!');
+			break;
+	}
 }
