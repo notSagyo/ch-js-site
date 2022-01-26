d7443196237b19f94c7e4ad86111f88734d879d1
Sagyo <notsagyo@gmail.com>
2022-01-26 18:39:16 -0300

    Add arrays, 'interactive' cart, type safety

--- js/cart.js
+++ js/cart.js
@@ -1,92 +1,100 @@
+// TODO: convert to ES6 classes?
+// TODO: manipulate quantity from cart
+// TODO: add find item method
 // Stores cart status and (todavia no) item list
-const Cart = function({discount = 0, payments = 1, taxRate: monthTaxRate = 0.03} = {}) {
+const Cart = function({discount, payments, monthlyInterest} = {}) {
+	let _itemList = [];
 	let _productCount = 0;
 	let _itemQuantity = 0;
 
 	let _total = 0;
 	let _subtotal = 0;
 	let _discount = discount || 0;
-	let _monthTaxRate = monthTaxRate || 0.03;
+	let _monthInterest = monthlyInterest || 0.03;
 	let _payments = payments || 1;
 	let _monthFee = 0;
 
-	// Get / Set -------------------- //
-	this.getTotal = () => { return _total; }
-	this.setTotal = (amount) => {
-		console.warn('Warning: you should not manually set the total.');
-		_total = amount;
-		return _total;
-	 }
-
-	this.getSubtotal = () => { return _subtotal; }
-	this.setSubtotal = (amount) => {
-		_subtotal = amount;
-		this.updatePrice();
-		return this.subtotal;
-	}
-
-	this.getDiscount = () => { return _discount; }
-	this.setDiscount = (amount) => {
-		_discount = amount;
-		this.updatePrice();
-		return _discount;
-	}
-
-	this.getPayments = () => { return _payments; }
-	this.setPayments = (amount) => {
-		_payments = amount;
-		this.updatePrice();
-		return _payments;
-	}
-
-	this.getTaxRate = () => { return _monthTaxRate };
-	this.setTaxRate = (amount) => {
-		_monthTaxRate = amount;
-		this.updatePrice();
-		return _monthTaxRate;
-	}
-
-	this.getMonthFee = () => { return _monthFee; }
-	this.getItemCount = () => { return _productCount; }
-	this.getItemQuantity = () => { return _itemQuantity; }
-
-	// Item methods ----------------- //
+	//#region Item Methods ---------- //
 	// Add item to cart
 	this.addItem = (item) => {
-		if (debugMode) console.log(`${item.name} added to cart`);
+		if (!(item instanceof CartItem)) {
+			console.error('Invalid arguments: CartItem expected');
+			return;
+		} else if (item.getQuantity() < 1) {
+			console.warn('Warning: zero quantity items won\'t be added');
+			return;
+		}
+
+		_itemList.push(item);
 		_productCount++;
 		_itemQuantity += item.getQuantity();
-		this.subtotalAdd(item.getTotal());
+
+		if (debugMode)
+			console.log(`%cAdded to cart: ${item.name} ` +
+				`(${item.getQuantity()})`,
+				'color: yellowgreen;');
+		this.updateCart();
 		return item;
+
 	}
 
-	// Remove item from cart
+	// Remove item from cart (CartItem or name accepted)
 	this.removeItem = (item) => {
-		if (debugMode) console.log(`${item.name} removed from cart`);
-		_productCount--;
-		_itemQuantity -= item.getQuantiy();
-		this.subtotalSubtract(item.getTotal());
-		return item;
+		let itemName = item;
+		let removedItem = item;
+		let removedIndex = 0;
+
+		if (item instanceof CartItem) {
+			itemName = item.name;
+			removedIndex = _itemList.indexOf(item);
+		} else if (typeof item == "string") {
+			removedItem = _itemList.find(x => x.name.toLowerCase() == itemName);
+			removedIndex = _itemList.indexOf(removedItem);
+		} else {
+			console.error('Invalid arguments: CartItem or string expected');
+			return;
+		}
+		if (removedIndex == -1) {
+			console.warn('Warning: item not found, no item removed ');
+			return;
+		}
+
+		_itemQuantity -= removedItem.getQuantity();
+		_itemList.splice(removedIndex, 1);
+
+		if (debugMode) {
+			console.log(
+				`%cRemoved from cart: ${itemName} ` +
+				`(${removedItem.getQuantity()})`,
+				'color: salmon;');
+		}
+		this.updateCart();
+		return removedItem;
 	}
 
 	// Remove all items from cart
 	this.clearCart = () => {
-		if (debugMode) console.log(`Cart was cleared`);
-		_productCount = 0;
 		_itemQuantity = 0;
-		this.setSubtotal(0);
+		_itemList = [];
+		if (debugMode) console.log('%cCART CLEARED', 'color: salmon;');
+		this.updateCart();
 	}
 
-	// Price methods ---------------- //
-	// After a variable set, update all variables
-	this.updatePrice = () => {
-		_total = _subtotal + this.calcTax(_payments, _monthTaxRate);
+	// After modifying a field, should call this
+	this.updateCart = () => {
+		_subtotal = _itemList.reduce((a, b) => a + b.getTotal(), 0);
+		_productCount = _itemList.length;
+		_total = _subtotal + this.calcInterest(_payments, _monthInterest);
 		_total = this.calcDiscount(_discount);
 		_monthFee = _total / _payments;
-		if (debugMode) console.log(`Cart price is now: $${this.getTotal()}`);
+
+		if (debugMode)
+			console.log(`Cart price now is: $${this.getTotal()}`);
 		return this.total;
 	}
+	//#endregion
 
+	//#region Price Methods --------- //
 	// Add amount to subtotal
 	this.subtotalAdd = (amount) => {
 		this.setSubtotal(_subtotal + amount)
@@ -103,88 +111,152 @@ const Cart = function({discount = 0, payments = 1, taxRate: monthTaxRate = 0.03}
 	}
 
 	// Calc. the tax ($) for the current subtotal
-	this.calcTax = (payments = _payments, tax = _monthTaxRate) => {
-		return (payments > 1) ? (_subtotal * tax * (payments - 1)) : 0;
+	this.calcInterest = (payments = _payments, interest = _monthInterest) => {
+		return (payments > 1) ? (_subtotal * interest * (payments - 1)) : 0;
 	}
 
 	// Calc. the monthly tax ($) for the current subtotal
-	this.calcMonthTax = (payments = _payments, tax = _monthTaxRate) => {
-		return this.calcTax(payments, tax) / payments;
+	this.calcMonthInterest = (payments = _payments, tax = _monthInterest) => {
+		return this.calcInterest(payments, tax) / payments;
 	}
+	//#endregion
+
+	// TODO: get set of _itemList
+	//#region Get / Set ------------- //
+	this.setItemList = (items) => {
+		if (!Array.isArray(items) || !items.every(x => x instanceof CartItem)) {
+			console.error('Invalid arguments: CartItems[] expected');
+			return;
+		}
+		_itemList = items;
+		this.updateCart();
+		return _itemList;
+	}
+
+	this.setTotal = (amount) => {
+		console.warn('Warning: total should not be manually set.');
+		_total = amount;
+		return _total;
+	}
+
+	this.setSubtotal = (amount) => {
+		_subtotal = amount;
+		this.updateCart();
+		return _subtotal;
+	}
+
+	this.setDiscount = (amount) => {
+		_discount = amount;
+		this.updateCart();
+		return _discount;
+	}
+
+	this.setMonthInterest = (amount) => {
+		_monthInterest = amount;
+		this.updateCart();
+		return _monthInterest;
+	}
+
+	this.setPayments = (amount) => {
+		_payments = amount;
+		this.updateCart();
+		return _payments;
+	}
+
+	this.getItems = () => { return _itemList; }
+	this.getProductCount = () => { return _productCount; }
+	this.getItemQuantity = () => { return _itemQuantity; }
+
+	this.getTotal = () => { return _total; }
+	this.getSubtotal = () => { return _subtotal; }
+	this.getDiscount = () => { return _discount; }
+	this.getMonthInterest = () => { return _monthInterest };
+	this.getPayments = () => { return _payments; }
+	this.getMonthFee = () => { return _monthFee; }
+	//#endregion
 
 	// Other ------------------------ //
 	// Print the object in readable format
 	this.toString = (multiline = false) => {
 		let str =
-			`productCount: ${this.getItemCount()}, ` +
-			`itemQuantity: ${this.getItemQuantity()}, ` +
-			`total: $${this.getTotal().toFixed(2)}, ` +
-			`subtotal: $${this.getSubtotal().toFixed(2)}, ` +
-			`discount: ${(this.getDiscount() * 100).toFixed(2)}%, ` +
-			`taxRate: ${(this.getTaxRate() * 100).toFixed(2)}%, ` +
-			`payments: ${this.getPayments()}, ` +
-			`monthFee: $${this.getMonthFee().toFixed(2)}`
-
+			`productCount: ${_productCount}, ` +
+			`itemQuantity: ${_itemQuantity}, ` +
+			`total: $${_total.toFixed(2)}, ` +
+			`subtotal: $${_subtotal.toFixed(2)}, ` +
+			`discount: ${(_discount * 100).toFixed(2)}%, ` +
+			`monthInterest: ${(_monthInterest * 100).toFixed(2)}%, ` +
+			`payments: ${_payments}, ` +
+			`monthFee: $${_monthFee.toFixed(2)}, `;
 		if (multiline) str = str.replace(/, /g, '\n');
+
+		let itemNames = [];
+		_itemList.forEach(item => itemNames.push(item.name));
+		itemNames = itemNames.join(', ');
+		str += `items: ${itemNames}`;
+
 		return str;
 	}
 };
 
 // Stores product related info
+// TODO: data validation
 const CartItem = function(name = 'Item', price = 0, quantity = 1) {
 	this.name = name;
 	let _price = price;
 	let _quantity = quantity;
 	let _total = price * quantity;
 
-	// Get / Set -------------------- //
-	this.getTotal = () => { return _total; }
-
-	this.getQuantity = () => { return _quantity; }
-	this.setQuantity = (amount) => {
-		_quantity = amount;
-		this.updateTotal();
-		return _quantity;
-	}
-
-	// Quantity methods ------------- //
+	//#region Quantity methods ------ //
 	this.increaseQuantity = (amount) => {
-		_quantity += amount || 1;
-		this.updateTotal();
-		return _quantity;
+		return this.setQuantity(_quantity + (amount || 1));
 	}
 
 	this.decreaseQuantity = (amount) => {
-		_quantity -= amount || 1;
-		this.updateTotal();
-		return _quantity;
+		return this.setQuantity(_quantity + (amount || -1));
 	}
 
 	this.resetQuantity = () => { return this.setQuantity(1); }
+	//#endregion
 
 	// Price methods ---------------- //
 	this.updateTotal = () => {
 		_total = _price * _quantity;
 		return _total;
 	}
+
+	//#region Get / Set ------------- //
+	this.setPrice = (amount) => {
+		if (!Number.isInteger(amount) || amount < 0) {
+			console.error('Invalid arguments: positive integer expected');
+			return;
+		}
+		_price = amount;
+		this.updateTotal();
+		return _price;
+	}
+
+	this.setQuantity = (amount) => {
+		if (!Number.isInteger(amount) || amount < 0) {
+			console.error('Invalid arguments: positive integer expected');
+			return;
+		}
+		_quantity = amount;
+		this.updateTotal();
+		return _quantity;
+	}
+
+	this.setTotal = (amount) => {
+		if (!Number.isInteger(amount) || amount < 0) {
+			console.error('Invalid arguments: positive integer expected');
+			return;
+		}
+		console.warn('Warning: total should not be manually set.');
+		_total = amount;
+		return _total;
+	}
+
+	this.getPrice = () => { return _price }
+	this.getQuantity = () => { return _quantity; }
+	this.getTotal = () => { return _total; }
+	//#endregion
 };
-
-// Log extra info if debug
-let debugMode = true;
-
-// Create some new items
-let shirt = new CartItem('T-Shirt', 20);
-shirt.setQuantity(10);
-let jeans = new CartItem('Jeans', 40);
-jeans.increaseQuantity(4);
-let shoes = new CartItem('Shoes', 55);
-shoes.increaseQuantity();
-
-// Create a new cart
-let cart = new Cart();
-cart.addItem(shirt);
-cart.addItem(jeans);
-cart.addItem(shoes);
-
-// Print cart status
-console.log(cart.toString(true));
