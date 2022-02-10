44a156aa8f36d6d2216d57ea48fe4b8365a24875
Sagyo <notsagyo@gmail.com>
2022-02-09 23:14:17 -0300

    Major refactor

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,5 @@
 // Create new cart and product list
-let productList = new ProductList();
+let clothesList = new ProductList();
 let cart = new Cart();
 
 // Set the active cart to the new cart
@@ -8,23 +8,22 @@ activeCart = cart;
 //#region Example ----------------------------------------------------------- //
 // Create some products
 let shirt = new Product('T-Shirt', 15, 'This is a simple T-Shirt.',
-	'..//img/shirt.webp');
+	'../img/shirt.webp');
 let jeans = new Product('Jeans', 30, 'These are simple Jeans.',
-	'..//img/jeans.webp');
+	'../img/jeans.webp');
 let shoes = new Product('Shoes', 50, 'These are simple Shoes.',
-	'..//img/shoes.webp');
+	'../img/shoes.webp');
 let belt = new Product('Belt', 20, 'This is a simple Belt.',
-	'..//img/belt.webp');
+	'../img/belt.webp');
 let shirt2 = new Product('Another T-Shirt', 15, 'Most goated T-Shirt.',
-	'..//img/shirt.webp', 2);
+	'../img/shirt.webp', 2);
 let prods = [shirt, jeans, shoes, belt];
 
 // Update the product list
-productList.setProducts(prods);
+clothesList.setProducts(prods);
 
 cart.setItemList(prods.concat([shirt2]));
 //#endregion
 
 // Log cart status
 cart.log();
-console.log(localStorage.getItem('test'));
