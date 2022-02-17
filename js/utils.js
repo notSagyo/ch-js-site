faf222d5960e2623ed4b8866684ffb208d882726
Sagyo <notsagyo@gmail.com>
2022-02-17 15:09:35 -0300

    Animation follow mouse only on md-up

--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,12 @@
 // Log extra info if debug
 let debugMode = true;
 
+// Media queries
+let mediaSm = 600;
+let mediaMd = 992;
+let mediaLg = 1200;
+let mediaXl = 1400;
+
 // Colors
 let colors = {
 	info: 'steelblue',
