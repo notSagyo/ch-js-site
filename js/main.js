997b999943a723126c595a1fe92ecc7355c1e742
Sagyo <notsagyo@gmail.com>
2022-02-19 06:13:50 -0300

    Add products qty manipulation

--- js/main.js
+++ js/main.js
@@ -1,18 +1,15 @@
 // Create new product list
 let clothesList = new ProductList();
+activeProductList = clothesList;
 
 // Create some products
-let shirt = new Product('T-Shirt', 15, 'This is a simple T-Shirt.',
-	'../img/shirt.webp');
-let jeans = new Product('Jeans', 30, 'These are simple Jeans.',
-	'../img/jeans.webp');
-let shoes = new Product('Shoes', 50, 'These are simple Shoes.',
-	'../img/shoes.webp');
-let belt = new Product('Belt', 20, 'This is a simple Belt.',
-	'../img/belt.webp');
-let shirt2 = new Product('Another T-Shirt', 15, 'Most goated T-Shirt.',
-	'../img/shirt.webp', 2);
-let prods = [shirt, jeans, shoes, belt];
+let prods = [
+	new Product('Shirt', 15, 'This is a simple Shirt.', '../img/shirt.webp'),
+	new Product('Jeans', 30, 'These are simple Jeans.', '../img/jeans.webp'),
+	new Product('Shoes', 50, 'These are simple Shoes.', '../img/shoes.webp'),
+	new Product('Belt', 20, 'This is a simple Belt.', '../img/belt.webp'),
+	new Product('Goated Shirt', 75, 'Most goated T-Shirt.', '../img/shirt.webp')
+];
 
 // Update the product list
 clothesList.setProducts(prods);
