e8db5424922bbe89ba25fe1aeb497710b7ec6d94
Sagyo <notsagyo@gmail.com>
2022-03-01 18:12:48 -0300

    Add about section

--- js/animation.js
+++ js/animation.js
@@ -13,47 +13,51 @@
 		heroLeft = '#hero-img-left',
 		heroRight = '#hero-img-right';
 
-	// Hero order images animation
-	gsap.from(heroLeft, { x: '-100%' });
-	gsap.from(heroRight, { x: '100%' });
-
 	//#region Logo animation ------------------------------------------------ //
-	// Hide cursor
-	gsap.to(line, { duration: 0, opacity: 0 });
+	if (document.querySelector(logo)) {
+		// Hide cursor
+		gsap.to(line, { duration: 0, opacity: 0 });
 
-	// Logo anim
-	tl.to(logo, { opacity: 1 }, 0)
-		.to([box, upper], { x: '-115px' })
-		.to(lower, { x: '115px' }, '<')
-		.to(upper, { y: '47px', duration: .75 }, '>')
-		.to(lower, { y: '-43px', duration: .75 }, '<')
-		.totalDuration(1.75);
+		// Logo anim
+		tl.to(logo, { opacity: 1 }, 0)
+			.to([box, upper], { x: '-115px' })
+			.to(lower, { x: '115px' }, '<')
+			.to(upper, { y: '47px', duration: .75 }, '>')
+			.to(lower, { y: '-43px', duration: .75 }, '<')
+			.totalDuration(1.75);
 
-	// Cursor anim
-	gsap.set(line, { strokeWidth: 4 });
-	tl = gsap.timeline({ repeat: -1, yoyo: true, delay: tl.totalDuration() * 0.75 })
-		.to(line, { opacity: 0, duration: 0.25 })
-		.to(line, { opacity: 1, duration: 0.5 });
-	//#endregion
+		// Cursor anim
+		gsap.set(line, { strokeWidth: 4 });
+		tl = gsap.timeline({ repeat: -1, yoyo: true, delay: tl.totalDuration() * 0.75 })
+			.to(line, { opacity: 0, duration: 0.25 })
+			.to(line, { opacity: 1, duration: 0.5 });
+		//#endregion
+	}
 
 	//#region Background animation ------------------------------------------ //
-	// Recuerdos de guerra de Algebra y hacer gráficos en Pascal
-	let screentCenterX = window.screen.width / 2;
-	let screenCenterY = window.screen.height / 2;
-	let bgForce = 0.05;
+	if (document.querySelector(heroBackground)) {
+		// Hero order images animation
+		gsap.from(heroLeft, { x: '-100%' });
+		gsap.from(heroRight, { x: '100%' });
 
-	if (window.screen.width > mediaMd) {
-		document.addEventListener('mousemove', (e) => {
-			let posX = (e.clientX - screentCenterX) * bgForce  + 'px';
-			let posY = (e.clientY - screenCenterY) * bgForce  + 'px';
+		// Recuerdos de guerra de Algebra y hacer gráficos en Pascal
+		let screentCenterX = window.screen.width / 2;
+		let screenCenterY = window.screen.height / 2;
+		let bgForce = 0.05;
 
-			gsap.to(heroBackground, {
-				backgroundPositionX: posX,
-				backgroundPositionY: posY,
-				duration: 0.5,
-				ease: 'ease-in-out'
+		if (window.screen.width > mediaMd) {
+			document.addEventListener('mousemove', (e) => {
+				let posX = (e.clientX - screentCenterX) * bgForce  + 'px';
+				let posY = (e.clientY - screenCenterY) * bgForce  + 'px';
+
+				gsap.to(heroBackground, {
+					backgroundPositionX: posX,
+					backgroundPositionY: posY,
+					duration: 0.5,
+					ease: 'ease-in-out'
+				});
 			});
-		});
+		}
 	}
 	//#endregion
 })();
