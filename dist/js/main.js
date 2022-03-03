3a2f23651992975df123f6e43d68d3f9a2e56433
Sagyo <notsagyo@gmail.com>
2022-03-02 21:24:42 -0300

    Update to fetch products from products.json

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,34 +1,33 @@
 (() => {
-	// Create new product list
+	// Create new product list and add some prods
+	let products = [];
 	let productList = new ProductList();
 	activeProductList = productList;
 
-	// Add some prods
-	let prods = [];
-
-	// TODO: put these in a JSON file
+	// TODO: make this a function
 	// Prods in case fetch request fails
-	prods = [
-		new Product('Shirt', 15,
-			'This is a simple shirt! If you\'re still reading this the products'
-			+ ' API is taking too long or maybe even failed! 🙃',
-			'../img/shirt.webp' ),
-		new Product('Jeans', 30, 'These are simple Jeans.', '../img/jeans.webp'),
-		new Product('Shoes', 50, 'These are simple Shoes.', '../img/shoes.webp'),
-		new Product('Belt', 20, 'This is a simple Belt.', '../img/belt.webp'),
-		new Product('Goated Shirt', 75, 'Most goated T-Shirt.', '../img/shirt.webp')
-	];
-	productList.setProducts(prods);
+	fetch('/dist/data/products.json')
+		.then(res => res.json())
+		.then(json =>  {
+			products = [];
+			json.forEach(product => {
+				let { title, price, description, image, category } = product;
+				products.push(new Product(title, price, description, image, category));
+			});
+			productList.setProducts(products);
+		})
+		.catch((err) => console.error(err));
 
+	// Products API request
 	fetch('https://fakestoreapi.com/products')
 		.then(res => res.json())
 		.then(json =>  {
-			prods = [];
-			json.forEach(prod => {
-				let { title, price, description, image, category } = prod;
-				prods.push(new Product(title, price, description, image, category));
+			products = [];
+			json.forEach(product => {
+				let { title, price, description, image, category } = product;
+				products.push(new Product(title, price, description, image, category));
 			});
-			productList.setProducts(prods);
+			productList.setProducts(products);
 		})
 		.catch((err) => console.error(err));
 })();
